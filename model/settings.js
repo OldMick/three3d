9848f94315c409150e0aca63b3f2079e63a31481
anhnguyenngocphan <anhnguyenngocphan@admicro.vn>
2018-09-22 11:31:38 +0700

    init project

---
+++ model/settings.js
@@ -0,0 +1,11 @@
+{
+	"scale":"0.07", 
+	"color":"#aaaaaa",
+	"reflectivity":"0.2",
+	"shininess":"0", 
+	"rotateSpeed":"0.1",
+	"direction":"-1",
+	"removeChildIndices": [0],
+	"colorrects":[[450,400,530,300],[305,718,719,306]],
+	"imagerect":[100,50,925,300]
+}
---
+++ settings.js
@@ -0,0 +1,13 @@
+{
+	"background": "images/bg7.jpg",
+	"backgrounds": ["bg1","bg2","bg4","bg5","bg6","bg7","bg8","bg9", "addimage"],
+	"objectColors":  [{"color1":"#ffffff","color2":"#ffffff"},
+	                  {"color1":"#eeeeee","color2":"#E80C0C"},
+					  {"color1":"#eeeeee","color2":"#0CB1F2"},
+					  {"color1":"#eeeeee","color2":"#E8CE0C"},
+					  {"color1":"#eeeeee","color2":"#32F20C"},
+					  {"color1":"#333333","color2":"#eeeeee"},
+					  {"color1":"#333333","color2":"#333333"}],
+	"model": "model",
+	"texture" : "images/wrapup.png"
+}
